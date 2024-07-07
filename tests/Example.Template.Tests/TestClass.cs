ab880a700b23982842579526fec70ad31f362898
Igor Chakrygin <igor@chakrygin.ru>
2024-07-07 14:28:10 +0300

    Update Example.Template.Tests project

---
+++ tests/Example.Template.Tests/TestClass.cs
@@ -0,0 +1,13 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Example.Template.Tests;
+
+[TestClass]
+public sealed class TestClass
+{
+    [TestMethod]
+    public void TestMethod()
+    {
+        Console.WriteLine("Hello, Example.Template!");
+    }
+}
